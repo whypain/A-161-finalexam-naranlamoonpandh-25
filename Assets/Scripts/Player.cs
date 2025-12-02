167b4d3fc781c12cb42212fbc2acdafffa9b6f3d
whypain <painingit@gmail.com>
2025-12-02 09:51:35 +0700

    feat: player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,93 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float MoveSpeed { get => moveSpeed; private set => moveSpeed = value; }
+    public float JumpForce { get => jumpForce; private set => jumpForce = value; }
+
+    public bool IsInvulnerable { get; private set; }
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce;
+
+    private Rigidbody2D rb;
+
+
+    public void SetMoveSpeed(float moveSpeed)
     {
-        
+        MoveSpeed = moveSpeed;
+        Debug.Log($"[Player] MoveSpeed: {MoveSpeed}");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetInvulnerability(bool isInvulnerable)
     {
-        
+        IsInvulnerable = isInvulnerable;
+        Debug.Log($"[Player] Invulnerability: {IsInvulnerable}");
+    }
+
+
+
+    public void Move()
+    {
+
+    }
+
+    public void Jump()
+    {
+
+    }
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2(input * MoveSpeed, rb.linearVelocityY);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpForce);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsInvulnerable) return;
+    }
+}
+
+public abstract class PowerUpBase
+{
+    protected bool IsEffectActive;
+
+    public abstract void ApplyEffect(Player player);
+}
+
+
+public class SpeedBoost : PowerUpBase
+{
+    public override void ApplyEffect(Player player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class ShieldBuff : PowerUpBase
+{
+    public override void ApplyEffect(Player player)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+public class Milk : PowerUpBase
+{
+    public override void ApplyEffect(Player player)
+    {
+        throw new System.NotImplementedException();
     }
 }
